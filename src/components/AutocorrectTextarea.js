3a979da692bfbd2712dbb49b8a97adf62bfb37af
remigius.kalimba <remigius.kalimba@flexperto.com>
2022-02-10 09:34:34 +0100

    Dont have enough time

--- src/components/AutocorrectTextarea.js
+++ src/components/AutocorrectTextarea.js
@@ -1,14 +1,33 @@
 import React from 'react';
 
 class AutocorrectTextarea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {textArea: "Ford"};
+    this.handler = this.handler.bind(this);
+  }
+
+  handler(evt) {
+    let inputArr = evt.target.value.split(' ')
+    this.setState({ textArea: evt.target.value })
+    const corrections = this.props.corrections;
+    let keys = Object.keys(corrections)
+    keys.map((key, _) => {
+      if (inputArr.includes(key)){
+        console.log(corrections[key]);
+      }
+    })
+
+  }
 
   render() {
     return (
       <div className="text-center">
-        <textarea data-testid="textarea" rows={10} cols={80} className="card" />
+        <textarea data-testid="textarea" rows={10} cols={80} className="card" value={this.state.textArea} onChange={this.handler} />
       </div>
     );
   }
 }
 
 export default AutocorrectTextarea;
+
